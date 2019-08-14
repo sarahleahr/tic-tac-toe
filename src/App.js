eacef578c4ea118c17f4cd1f6abdb52720f10c9e
Olivier Amouyal <olivier.amouyal@gmail.com>
2019-08-14 20:38:47 +0300

    built tic-tac-toe

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const combos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const calculateWinner = board =>
+  combos.map(combo => combo.map(cell => board[cell]))
+        .map(boardCombo => (
+          boardCombo.reduce((winner, cell)=>
+          winner === cell ? winner : null,
+          boardCombo[0]
+        )
+      )).filter(winner => winner)[0];
+
+const makeMove = (board, clicked) => {
+  if (board[clicked]) return board;
+
+  const xs = board.filter(cell => cell == "X").length;
+  const os = board.filter(cell => cell == "O").length;
+  const nextMove = (xs === os) ? 'X' : 'O';
+
+  let nextBoard = board.map((cell, i) => (
+    (i === clicked) ? nextMove : cell
+  ));
+
+  return nextBoard;
+};
+
 function App() {
+  const [board, setBoard] = useState([
+    null, null, null,
+    null, null, null,
+    null, null, null,
+  ]);
+
+  const currentWinner = calculateWinner(board);
+
+  if( currentWinner ) return [...Array(100)].map((o, i)=> (
+    <span style={{
+      left: (Math.random()*99)+'vw',
+      animationDelay: (Math.random()*5)+'s',
+      animationDuration: (4 + Math.random()*3)+'s',
+    }}>{currentWinner}</span>
+  ));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        board.map((cell, i) => (
+        <div className="cell" onClick={ ()=> setBoard(makeMove(board, i)) }>
+            {cell}
+        </div>
+
+        ))
+      }
     </div>
   );
 }
